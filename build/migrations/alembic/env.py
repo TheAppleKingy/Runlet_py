d3f422332f7ad6d7955284607f6127ce0373c112
TheAppleKingy <pridanov.ali.job@gmail.com>
2026-01-30 00:29:37 +0600

    Brief description of changes: Add DB config copy and update Alembic env to use Config class; add Pydantic settings dependency.
    added:
    - COPY ./src/infrastructure/configs.py in Dockerfile
    - import and instantiate DBConfig in alembic/env.py
    - new @property methods in DBConfig and RabbitMQConfig
    - new package entries in poetry.lock (annotated-types, pydantic, pydantic-core)
    - pydantic-settings added to pyproject.toml dependencies
    
    removed:
    - runlet_rabbitmq service and volume definitions in compose.yaml
    - parentheses in DBProvider.get_engine method call
    
    modified:
    - config.set_main_option URL source from env var to DBConfig.conn_url
    - DBProvider.get_engine to use config.conn_url without parentheses
    
    moved:
    
    Profit Updated DB config handling and added Pydantic settings support.

--- build/migrations/alembic/env.py
+++ build/migrations/alembic/env.py
@@ -8,6 +8,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from src.infrastructure.db.tables import *
+from src.infrastructure.configs import DBConfig
+
+conf = DBConfig()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +20,7 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
+config.set_main_option("sqlalchemy.url", conf.conn_url)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
