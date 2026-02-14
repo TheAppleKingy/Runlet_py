22cda5d53aa96148f7ebd2e1d259dc7476bf7f33
TheAppleKingy <pridanov.ali.job@gmail.com>
2026-02-14 21:51:39 +0700

    hotfix

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -1,8 +1,4 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
 from dataclasses import dataclass, field
-if TYPE_CHECKING:
-    from .course import Course
 
 
 @dataclass
@@ -12,4 +8,3 @@ class User:
     name: str = ""
     is_active: bool = field(default=False, init=False)
     id: int = field(default=None, init=False)  # type: ignore
-    tags: list[Course] = field(default_factory=list, init=False)
