920639f9a0a674c0d35e8cb8aa148c319d9e8005
TheAppleKingy <pridanov.ali.job@gmail.com>
2026-03-22 21:59:21 +0700

    Brief description ofchanges: Add student course unsubscribe feature with email notification and HTTP endpoint.
    added:
    - UnsubscribeFromCourse class
    - UnsubscribeFromCourse import and __all__ entry
    - EmailMessageTextTemplate.notify_student_unsubscribed method
    - InteractorProvider.unsubscribe_from_course provider- HTTP route and controller for unsubscription
    - Additional imports of UnsubscribeFromCourse in student module
    
    removed:
    -
    
    modified:
    -
    
    moved:
    -

--- runlet/application/interactors/student.py
+++ runlet/application/interactors/student.py
@@ -4,8 +4,10 @@ from runlet.domain.entities import (
     Course,
     Module,
     Problem,
-    Attempt
+    Attempt,
+    Tag
 )
+from runlet.domain.services import CourseStudentsManagerService
 from runlet.application.interfaces.repositories import (
     CourseRepositoryInterface,
     AttemptRepositoryInterface
@@ -22,9 +24,13 @@ from runlet.application.dtos.student import (
     TestSolutionDTO
 )
 from .base import (
-    _CourseAttemptReposRelatedInteractor
+    _CourseAttemptReposRelatedInteractor,
+    _CourseUserReposRelatedInteractor
 )
-
+from runlet.application.interfaces.services import (
+    EmailMessageTextTemplate,
+    EmailServiceInterface
+)
 from runlet.application.interfaces.message_tasks import SendCodeSolution
 
 
@@ -101,3 +107,19 @@ class SendProblemSolution(_CourseAttemptReposRelatedInteractor):
                 run_data=[RunDataDTO(test_num=num, input=case.input) for num, case in problem.test_cases]
             )
             await self._publisher.publish(SendCodeSolution(message_dto.model_dump_json()))
+
+
+class UnsubscribeFromCourse(_CourseUserReposRelatedInteractor):
+    def __init__(self, uow, course_repo, user_repo, email_service: EmailServiceInterface, main_url: str):
+        super().__init__(uow, course_repo, user_repo)
+        self._email_service = email_service
+        self._main_url = main_url
+
+    async def __call__(self, student_id: int, course_id: int):
+        async with self._uow:
+            course = await self._course_repo.get_by_id_with_rels(course_id, [Course._tags, Tag.students], [Course._students])
+            manager = CourseStudentsManagerService(course)
+            manager.delete_students([student_id])
+            student = await self._user_repo.get_by_id(student_id)
+        topic, message = EmailMessageTextTemplate.notify_student_unsubscribed(course.name, self._main_url)
+        await self._email_service.send_mail(student.email, topic, message)
